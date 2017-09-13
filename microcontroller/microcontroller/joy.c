94dcbb6481f457cbcaa469578c43d7387339f3f1
Tobias Bergkvist <tobias@bergkv.ist>
2017-09-13 17:51:36 +0200

    Add joy_getDirection and joy_getPosition.

--- microcontroller/microcontroller/joy.c
+++ microcontroller/microcontroller/joy.c
@@ -10,10 +10,13 @@
 #include "joy.h"
 #include "defines.h"
 
+#define X_COORD 0
+#define Y_COORD 1
+
 struct Coordinate {
 	uint8_t x;
 	uint8_t y;
-} max, min;
+} max, min, rad, mid;
 
 
 void joy_calibrate(){
@@ -37,6 +40,47 @@ void joy_calibrate(){
 		// Calibrate y
 		if (min.y > y) min.y = y;
 		else if (max.y < y) max.y = y;		
+	}
+
+	// Find distance from middle to edges (radius)
+	rad.x = (max.x - min.x) / 2;
+	rad.y = (max.y - min.y) / 2;
+	
+	// Find mid point using edges.
+	mid.x = (max.x + min.x) / 2;
+	mid.y = (max.y + min.y) / 2;
+}
+
+/**
+ * Calculate position (between -100 and 100) using current signal and values from calibration
+ * @returns {joy_position_t} - The x and y coordinates as values between -100 and 100
+ */
+joy_position_t joy_getPosition() {
+	joy_position_t joy_position;
+	joy_position.x = (100 * (adc_read(X_COORD) - mid.x)) / rad.x;
+	joy_position.y = (100 * (adc_read(Y_COORD) - mid.y)) / rad.y;
+	return joy_position;
+}
+
+joy_direction_t joy_getDirection() {
+	joy_position_t position = joy_getPosition();
+	
+	// The joystick is within {(-5, -5), (5, 5)}, and the position is considered neutral.
+	if (abs(position.x) < 5 && abs(position.y) < 5) return NEUTRAL;
+
+	// The x-coordinate is dominating
+	else if (abs(position.x) > abs(position.y)) {
+		// Positive x => RIGHT
+		if (position.x > 0) return RIGHT;
+		// Negative x => LEFT
+		else return LEFT;
+
+	// The y-coordinate is dominating
+	} else {
+		// Positive y => UP
+		if (position.y > 0) return UP;
+		// Negative y => DOWN
+		else return DOWN;
 	}
 }
 
